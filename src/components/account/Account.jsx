8b01da3be768d5f32fd37e97595e6c15dc575cdf
radoheritiana <radoheritiana.rhja.123@gmail.com>
2023-11-11 10:19:01 +0300

    add profil

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,13 +1,47 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 
 const Account = () => {
+  const [user, setUser] = useState({});
 
-    return (
-        <React.Fragment>
-            
-        </React.Fragment>
-    );
-}
+  useEffect(() => {
+    setUser(JSON.parse(sessionStorage.getItem("user")));
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div className="container mt-5 mb-5 pt-5 pb-5">
+        <div className="row mt-5">
+          <div className="col-md-12">
+            <h1>Mon profil</h1>
+            <hr style={{ borderColor: "gray" }} />
+            <div className="row">
+              <div className="col-md-8">
+                <h5>
+                  <b>Nom</b> : <i>{user.first_name}</i>
+                </h5>
+                <h5>
+                  <b>Prénoms</b> : <i>{user.last_name}</i>
+                </h5>
+                <h5>
+                  <b>Email</b> : <i>{user.email}</i>
+                </h5>
+              </div>
+              <div className="col-md-4">
+                <img
+                  src="https://www.w3schools.com/howto/img_avatar.png"
+                  alt="Avatar"
+                  className="avatar"
+                  style={{ width: "100%" }}
+                />
+              </div>
+            </div>
+            <hr style={{ borderColor: "gray" }} />
+            <button className="btn btn-yellow">Modifier</button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Account;
